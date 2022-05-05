67f1db9a3393f816411b647ee7f8394da656b3bc
František Gič <mcfreddie777@gmail.com>
2022-05-06 00:43:13 +0200

    Serialize create_at as timestamp

--- samples/serializers.py
+++ samples/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
+
 from samples.models import Sample
+
+
+class TimestampField(serializers.Field):
+    def to_representation(self, value):
+        return round(value.timestamp() * 1000)
 
 
 class SampleSerializer(serializers.ModelSerializer):
@@ -8,3 +14,4 @@ class SampleSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     available = serializers.ReadOnlyField()
+    created_at = TimestampField(required=False)
